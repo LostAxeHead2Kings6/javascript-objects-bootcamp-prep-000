ad7fbc04294808ef518b0788170d44e9c94e5428
Michael Vasios <mvasios1@gmail.com>
2017-12-02 19:55:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { genesis: 'Turn it on Again'}
 function updatePlaylist(obj, artist, song) {
   return Object.assign({}, obj, { [artist]: song })
 }
+
+function removeFromPlaylist(obj, artist) {
+  
+}
